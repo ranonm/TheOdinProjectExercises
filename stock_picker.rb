03e74e776282e65df15c1167167901ccf87e2afd
Ranon Martin <ranonmartin1@gmail.com>
2020-01-07 03:11:50 -0500

    Use 2 space indentations & remove explicit returns

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,25 +1,25 @@
 def stock_picker(prices)
-    max_profit = 0
-    buying_day = 0
-    while buying_day < (prices.count - 1)
-        selling_day = buying_day + 1
+  max_profit = 0
+  buying_day = 0
+  while buying_day < (prices.count - 1)
+    selling_day = buying_day + 1
 
-        while selling_day < prices.count
-            profit = prices[selling_day] - prices[buying_day]
+    while selling_day < prices.count
+      profit = prices[selling_day] - prices[buying_day]
 
-            if profit > max_profit
-                best_buying_day = buying_day
-                best_selling_day = selling_day
-                max_profit = profit
-            end
+      if profit > max_profit
+        best_buying_day = buying_day
+        best_selling_day = selling_day
+        max_profit = profit
+      end
             
-            selling_day += 1
-        end
-
-        buying_day += 1
+      selling_day += 1
     end
+
+    buying_day += 1
+  end
     
-    return [best_buying_day, best_selling_day]
+  [best_buying_day, best_selling_day]
 end
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
--- substrings.rb
+++ substrings.rb
@@ -1,10 +1,10 @@
 def substrings(phrase, valid_substrings)
-    substring_frequency = {}
-    valid_substrings.each do |substring|
-        frequency = phrase.downcase.scan(substring).length
-        substring_frequency[substring] = frequency if frequency > 0
-    end
-    return substring_frequency
+  substring_frequency = {}
+  valid_substrings.each do |substring|
+    frequency = phrase.downcase.scan(substring).length
+    substring_frequency[substring] = frequency if frequency > 0
+  end
+  substring_frequency
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
